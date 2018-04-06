fd73b78accab4eb0aeda2a69b5d9b763f0f9f93f
Joseph Wright <joseph@cloudboss.co>
2018-04-05 20:12:09 -0400

    Update copyright dates

--- pkg/whisper/whisper_test.go
+++ pkg/whisper/whisper_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Joseph Wright <joseph@cloudboss.co>
+// Copyright © 2018 Joseph Wright <joseph@cloudboss.co>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
